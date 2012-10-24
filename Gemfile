5ad27936ad2d4da4c610f4c1384480acba4b3738
Salomon Trujillo <salomonjtrujillo@gmail.com>
2012-10-24 16:24:29 -0700

    Finished static pages and Ruby primer

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.8'
+gem 'wirble', '0.1.3'
 
 group :development, :test do
   gem 'sqlite3', '1.3.5'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
   
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  
   describe "Home page" do
     
     it "should have the content 'Sample App'" do
@@ -11,8 +13,12 @@ describe "Static pages" do
     
     it "should have the right title" do
       visit '/static_pages/home'
-        page.should have_selector('title',
-            :text => "Ruby on Rails Tutorial Sample App | Home")
+      page.should have_selector('title',
+            :text => "#{base_title}")
+    end
+    it "should not have an agumented title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => "|")
     end
   end
   
@@ -26,7 +32,7 @@ describe "Static pages" do
     it "should have the right title" do
       visit '/static_pages/help'
         page.should have_selector('title',
-            :text => "Ruby on Rails Tutorial Sample App | Help")
+            :text => "#{base_title} | Help")
     end
   end
   
@@ -39,7 +45,19 @@ describe "Static pages" do
     it "should have the right title" do
       visit '/static_pages/about'
         page.should have_selector('title',
-            :text => "About Us")
+            :text => "#{base_title} | About Us")
+    end
+  end
+  
+  describe "Contact page" do
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+        :text => "#{base_title} | Contact")
     end
   end
 end
