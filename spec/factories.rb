6b71e69c21c3e9cfd409dd2df73343e0df1e900d
Salomon Trujillo <salomonjtrujillo@gmail.com>
2012-10-26 22:19:18 -0700

    Finished user signup

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Salomon Trujillo"
+    email    "sal@salomon.com"
+    password "1234567890"
+    password_confirmation "1234567890"
+  end
+end
+
